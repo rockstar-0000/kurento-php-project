6838896add31dc8e819cd17d300bb920e6eaab0b
rockstar-0000 <tekbrucejin@gmail.com>
2020-12-14 01:45:12 +0100

    Api for moving the recording file to the specific dir.

---
+++ server/server.js
@@ -0,0 +1,48 @@
+// Node imports
+var express = require('express');
+var fs = require('fs');
+var session = require('express-session');
+var https = require('https');
+var bodyParser = require('body-parser'); // Pull information from HTML POST (express4)
+var app = express(); // Create our app with express
+
+// Server configuration
+app.use(session({
+    saveUninitialized: true,
+    resave: false,
+    secret: 'MY_SECRET'
+}));
+app.use(bodyParser.urlencoded({
+    'extended': 'true'
+})); // Parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // Parse application/json
+app.use(bodyParser.json({
+    type: 'application/vnd.api+json'
+})); // Parse application/vnd.api+json as json
+
+// Listen (start app with node server.js)
+var options = {
+    key: fs.readFileSync('openvidukey.pem'),
+    cert: fs.readFileSync('openviducert.pem')
+};
+https.createServer(options, app).listen(443);
+console.log("App listening on port 443");
+
+/* REST API */
+app.post('/moveRecording', (req, res) => {
+    let data = req.body;
+    exec("./test.sh " + data.sessionName + " " + data.destDir, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log("delete path: ", data.sessionName)
+        console.log("delete path: ", data.destDir)
+        res.send(response);
+    });
+});
