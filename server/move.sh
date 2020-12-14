a14ab94a2bec018c7203ac995dcc7bb4b159d88a
rockstar-0000 <tekbrucejin@gmail.com>
2020-12-14 02:07:45 +0100

    Added moving script feature.

---
+++ server/move.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd "/record/$1"
+dest_dir=$2
+current_dir="$PWD"
+echo "Current Record path: $PWD"
+for f in *.mp4; do
+        if [ -f "$f" ] # does file exist?
+        then
+                mv "$f" "$dest_dir"     # move file into new dir
+        fi
+done
